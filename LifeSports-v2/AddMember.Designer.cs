ce118e226fc2516667e2a35b6b7d119f9fb0ba26
Florea,Victor V <v.victor@student.fontys.nl>
2020-03-23 20:21:33 +0100

    Project

---
+++ LifeSports-v2/AddMember.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace LifeSports_v2
+{
+    partial class AddMember
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxFName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxLName = new System.Windows.Forms.TextBox();
+            this.tbxEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbxDay = new System.Windows.Forms.TextBox();
+            this.tbxMonth = new System.Windows.Forms.TextBox();
+            this.tbxYear = new System.Windows.Forms.TextBox();
+            this.cbSwim = new System.Windows.Forms.CheckBox();
+            this.cbDance = new System.Windows.Forms.CheckBox();
+            this.cbBilliard = new System.Windows.Forms.CheckBox();
+            this.cbGym = new System.Windows.Forms.CheckBox();
+            this.cbHasPaid = new System.Windows.Forms.CheckBox();
+            this.btnAddMember = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxFName
+            // 
+            this.tbxFName.Location = new System.Drawing.Point(121, 8);
+            this.tbxFName.Name = "tbxFName";
+            this.tbxFName.Size = new System.Drawing.Size(100, 22);
+            this.tbxFName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First name: ";
+            // 
+            // tbxLName
+            // 
+            this.tbxLName.Location = new System.Drawing.Point(121, 36);
+            this.tbxLName.Name = "tbxLName";
+            this.tbxLName.Size = new System.Drawing.Size(100, 22);
+            this.tbxLName.TabIndex = 2;
+            // 
+            // tbxEmail
+            // 
+            this.tbxEmail.Location = new System.Drawing.Point(121, 64);
+            this.tbxEmail.Name = "tbxEmail";
+            this.tbxEmail.Size = new System.Drawing.Size(145, 22);
+            this.tbxEmail.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Last Name: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Day";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(118, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Month";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(203, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Year";
+            // 
+            // tbxDay
+            // 
+            this.tbxDay.Location = new System.Drawing.Point(24, 126);
+            this.tbxDay.Name = "tbxDay";
+            this.tbxDay.Size = new System.Drawing.Size(51, 22);
+            this.tbxDay.TabIndex = 9;
+            // 
+            // tbxMonth
+            // 
+            this.tbxMonth.Location = new System.Drawing.Point(114, 126);
+            this.tbxMonth.Name = "tbxMonth";
+            this.tbxMonth.Size = new System.Drawing.Size(51, 22);
+            this.tbxMonth.TabIndex = 10;
+            // 
+            // tbxYear
+            // 
+            this.tbxYear.Location = new System.Drawing.Point(192, 126);
+            this.tbxYear.Name = "tbxYear";
+            this.tbxYear.Size = new System.Drawing.Size(59, 22);
+            this.tbxYear.TabIndex = 11;
+            // 
+            // cbSwim
+            // 
+            this.cbSwim.AutoSize = true;
+            this.cbSwim.Location = new System.Drawing.Point(12, 169);
+            this.cbSwim.Name = "cbSwim";
+            this.cbSwim.Size = new System.Drawing.Size(124, 21);
+            this.cbSwim.TabIndex = 12;
+            this.cbSwim.Text = "Swimming Pool";
+            this.cbSwim.UseVisualStyleBackColor = true;
+            // 
+            // cbDance
+            // 
+            this.cbDance.AutoSize = true;
+            this.cbDance.Location = new System.Drawing.Point(168, 169);
+            this.cbDance.Name = "cbDance";
+            this.cbDance.Size = new System.Drawing.Size(114, 21);
+            this.cbDance.TabIndex = 13;
+            this.cbDance.Text = "Dance/ Ballet";
+            this.cbDance.UseVisualStyleBackColor = true;
+            // 
+            // cbBilliard
+            // 
+            this.cbBilliard.AutoSize = true;
+            this.cbBilliard.Location = new System.Drawing.Point(12, 196);
+            this.cbBilliard.Name = "cbBilliard";
+            this.cbBilliard.Size = new System.Drawing.Size(133, 21);
+            this.cbBilliard.TabIndex = 14;
+            this.cbBilliard.Text = "Billiard/ Snooker";
+            this.cbBilliard.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cbBilliard.UseVisualStyleBackColor = true;
+            // 
+            // cbGym
+            // 
+            this.cbGym.AutoSize = true;
+            this.cbGym.Location = new System.Drawing.Point(168, 196);
+            this.cbGym.Name = "cbGym";
+            this.cbGym.Size = new System.Drawing.Size(59, 21);
+            this.cbGym.TabIndex = 15;
+            this.cbGym.Text = "Gym";
+            this.cbGym.UseVisualStyleBackColor = true;
+            // 
+            // cbHasPaid
+            // 
+            this.cbHasPaid.AutoSize = true;
+            this.cbHasPaid.Location = new System.Drawing.Point(114, 235);
+            this.cbHasPaid.Name = "cbHasPaid";
+            this.cbHasPaid.Size = new System.Drawing.Size(58, 21);
+            this.cbHasPaid.TabIndex = 16;
+            this.cbHasPaid.Text = "Paid";
+            this.cbHasPaid.UseVisualStyleBackColor = true;
+            // 
+            // btnAddMember
+            // 
+            this.btnAddMember.Location = new System.Drawing.Point(12, 285);
+            this.btnAddMember.Name = "btnAddMember";
+            this.btnAddMember.Size = new System.Drawing.Size(270, 35);
+            this.btnAddMember.TabIndex = 17;
+            this.btnAddMember.Text = "Add member";
+            this.btnAddMember.UseVisualStyleBackColor = true;
+            // 
+            // AddMember
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 330);
+            this.Controls.Add(this.btnAddMember);
+            this.Controls.Add(this.cbHasPaid);
+            this.Controls.Add(this.cbGym);
+            this.Controls.Add(this.cbBilliard);
+            this.Controls.Add(this.cbDance);
+            this.Controls.Add(this.cbSwim);
+            this.Controls.Add(this.tbxYear);
+            this.Controls.Add(this.tbxMonth);
+            this.Controls.Add(this.tbxDay);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxEmail);
+            this.Controls.Add(this.tbxLName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxFName);
+            this.Name = "AddMember";
+            this.Text = "AddMember";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxFName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxLName;
+        private System.Windows.Forms.TextBox tbxEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbxDay;
+        private System.Windows.Forms.TextBox tbxMonth;
+        private System.Windows.Forms.TextBox tbxYear;
+        private System.Windows.Forms.CheckBox cbSwim;
+        private System.Windows.Forms.CheckBox cbDance;
+        private System.Windows.Forms.CheckBox cbBilliard;
+        private System.Windows.Forms.CheckBox cbGym;
+        private System.Windows.Forms.CheckBox cbHasPaid;
+        private System.Windows.Forms.Button btnAddMember;
+    }
+}
---
+++ LifeSports-v2/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace LifeSports_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxMembers = new System.Windows.Forms.ListBox();
+            this.lblSwim = new System.Windows.Forms.Label();
+            this.lblBilliard = new System.Windows.Forms.Label();
+            this.lblGym = new System.Windows.Forms.Label();
+            this.lblDance = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lblAccess = new System.Windows.Forms.Label();
+            this.tbCode = new System.Windows.Forms.TextBox();
+            this.lblCode = new System.Windows.Forms.Label();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxMembers
+            // 
+            this.lbxMembers.FormattingEnabled = true;
+            this.lbxMembers.ItemHeight = 16;
+            this.lbxMembers.Location = new System.Drawing.Point(13, 13);
+            this.lbxMembers.Name = "lbxMembers";
+            this.lbxMembers.Size = new System.Drawing.Size(189, 212);
+            this.lbxMembers.TabIndex = 0;
+            // 
+            // lblSwim
+            // 
+            this.lblSwim.BackColor = System.Drawing.Color.DimGray;
+            this.lblSwim.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblSwim.Location = new System.Drawing.Point(208, 13);
+            this.lblSwim.Name = "lblSwim";
+            this.lblSwim.Size = new System.Drawing.Size(135, 42);
+            this.lblSwim.TabIndex = 1;
+            this.lblSwim.Text = "SwimmingPool";
+            this.lblSwim.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBilliard
+            // 
+            this.lblBilliard.BackColor = System.Drawing.Color.DimGray;
+            this.lblBilliard.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblBilliard.Location = new System.Drawing.Point(208, 71);
+            this.lblBilliard.Name = "lblBilliard";
+            this.lblBilliard.Size = new System.Drawing.Size(135, 42);
+            this.lblBilliard.TabIndex = 2;
+            this.lblBilliard.Text = "Billiard/ Snooker";
+            this.lblBilliard.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGym
+            // 
+            this.lblGym.BackColor = System.Drawing.Color.DimGray;
+            this.lblGym.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblGym.Location = new System.Drawing.Point(208, 126);
+            this.lblGym.Name = "lblGym";
+            this.lblGym.Size = new System.Drawing.Size(135, 42);
+            this.lblGym.TabIndex = 3;
+            this.lblGym.Text = "Gym";
+            this.lblGym.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDance
+            // 
+            this.lblDance.BackColor = System.Drawing.Color.DimGray;
+            this.lblDance.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblDance.Location = new System.Drawing.Point(208, 183);
+            this.lblDance.Name = "lblDance";
+            this.lblDance.Size = new System.Drawing.Size(135, 42);
+            this.lblDance.TabIndex = 4;
+            this.lblDance.Text = "Dance/ Ballet";
+            this.lblDance.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(13, 231);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(163, 33);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add Member";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(180, 231);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(163, 33);
+            this.btnRemove.TabIndex = 6;
+            this.btnRemove.Text = "Remove Member";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // lblAccess
+            // 
+            this.lblAccess.BackColor = System.Drawing.Color.DimGray;
+            this.lblAccess.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblAccess.Location = new System.Drawing.Point(363, 71);
+            this.lblAccess.Name = "lblAccess";
+            this.lblAccess.Size = new System.Drawing.Size(194, 141);
+            this.lblAccess.TabIndex = 7;
+            this.lblAccess.Text = "Scan";
+            this.lblAccess.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbCode
+            // 
+            this.tbCode.Location = new System.Drawing.Point(406, 33);
+            this.tbCode.Name = "tbCode";
+            this.tbCode.Size = new System.Drawing.Size(110, 22);
+            this.tbCode.TabIndex = 8;
+            // 
+            // lblCode
+            // 
+            this.lblCode.AutoSize = true;
+            this.lblCode.Location = new System.Drawing.Point(436, 13);
+            this.lblCode.Name = "lblCode";
+            this.lblCode.Size = new System.Drawing.Size(45, 17);
+            this.lblCode.TabIndex = 9;
+            this.lblCode.Text = "Code:";
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(363, 226);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(194, 35);
+            this.btnScan.TabIndex = 10;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 273);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.lblCode);
+            this.Controls.Add(this.tbCode);
+            this.Controls.Add(this.lblAccess);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblDance);
+            this.Controls.Add(this.lblGym);
+            this.Controls.Add(this.lblBilliard);
+            this.Controls.Add(this.lblSwim);
+            this.Controls.Add(this.lbxMembers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxMembers;
+        private System.Windows.Forms.Label lblSwim;
+        private System.Windows.Forms.Label lblBilliard;
+        private System.Windows.Forms.Label lblGym;
+        private System.Windows.Forms.Label lblDance;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label lblAccess;
+        private System.Windows.Forms.TextBox tbCode;
+        private System.Windows.Forms.Label lblCode;
+        private System.Windows.Forms.Button btnScan;
+    }
+}
+
